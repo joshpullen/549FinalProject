0235f9d85e3e7786dfcf78f5a4c415e52fa67a77
joshpullen <jdpullen@wustl.edu>
2021-11-17 00:28:58 -0600

    added parallel merge sort

---
+++ sorters/ParallelMergeSort.h
@@ -0,0 +1,5 @@
+void ParallelMergeSort(int* input, int* output, int  length);
+
+void ParallelMerge(int* A, int* B, int* output, int Alen, int Blen);
+
+int BinarySearch(int* array, int value, int length);
